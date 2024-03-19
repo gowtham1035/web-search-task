e317a73f5a65825dd7fe6d65b26cd3a9fe4a3b80
gowtham1035 <gowthamneelam1@msitprogram.net>
2024-03-19 15:00:28 +0530

    unwanted code is removed

--- crawl.py
+++ crawl.py
@@ -1,13 +1,3 @@
-
-# from collections import defaultdict
-# from nltk.corpus import stopwords
-# from nltk.tokenize import word_tokenize
-# from urllib.request import urlopen
-# from urllib.parse import urlparse, urljoin
-# from bs4 import BeautifulSoup
-# import re
-# import csv
-
 class RankingAlgorithm:
     def __init__(self, indexer):
         self.indexer = indexer
@@ -23,27 +13,4 @@ class RankingAlgorithm:
                     scores[url] += 1
         
         return sorted(scores.items(), key=lambda x: x[1], reverse=True)
-
-class Indexer:
-    # Inverted index (words -> document URLs)
-    def __init__(self):
-        self.inverted_index = defaultdict(list)
-        # Stop words from NLTK (assumed imported)
-        self.stop_words = set(stopwords.words('english'))
-
-    # Clean and normalize text (lowercase, alnum, remove stop words)
-    def process_text(self, text):
-        words = word_tokenize(text)
-        words = [word.lower() for word in words if word.isalnum() and word.lower() not in self.stop_words]
-        return words
-
-    # Build index from text, optionally associate URL
-    def build_index_from_text(self, text, url=None):
-        words = self.process_text(text)
-        for word in words:
-            self.inverted_index[word].append(url)
-
-    # Get the built inverted index
-    def get_index(self):
-        return self.inverted_index
 
